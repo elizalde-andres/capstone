13755651685bb5960ab670c05d1e7f39233393a9
Andrés Elizalde <andres@MacBook-Pro-de-Andres.local>
2021-09-07 19:47:54 +0200

    Minor fixes

--- tests/static/tests/js/teacher_results.js
+++ tests/static/tests/js/teacher_results.js
@@ -4,16 +4,18 @@ let starting_part_question = 1
 let category_name = ""
 
 document.addEventListener('DOMContentLoaded', function () {
-
+    // Hide all the results tables
     document.querySelectorAll(".results-table").forEach(table => {
         table.style.display = 'none';
     });
 
+    // Set expand/collapse button style and content
     document.querySelectorAll(".expand-collapse").forEach(button => {
         button.addEventListener('click', () => {
             button.classList.toggle("expanded");
             if (button.classList.contains("expanded")){
                 button.innerHTML = '<div class="btn-box"><a class="page-link mt-0 px-3">Collapse answers <i class="fa fa-solid fa-chevron-up"></i></a></div>';
+                document.querySelector(`#results-${button.value}`).style.animationPlayState = 'running';
                 document.querySelector(`#results-${button.value}`).style.display = 'block';
             } else {
                 button.innerHTML = '<div class="btn-box"><a class="page-link mt-0 px-3">Expand answers <i class="fa fa-solid fa-chevron-down"></i></a></div>';
@@ -22,23 +24,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
         })
     });
-
-    document.querySelectorAll(".update-score-btn").forEach(button => {
-        button.addEventListener('click', () => {
-            assignment_id = button.value;
-            document.querySelectorAll(`.new-score-${assignment_id}`).forEach(async element => {
-                var answer_id = await element.id
-                var csrfToken = await document.getElementById("csrfToken").value
-                
-                await fetch(`/update_score/${assignment_id}/${answer_id}`, {
-                    method: 'PUT',
-                    headers:{'X-CSRFToken': csrfToken},
-                    body: JSON.stringify({
-                        new_score: element.value
-                    })
-                })
-            })
-            return false;
-        })
-    });
 })
+
+
+function update_score(assignment_id, test_id) {
+    document.querySelectorAll(`.new-score-${assignment_id}`).forEach(async element => {
+        var answer_id = await element.id
+        var csrfToken = await document.getElementById("csrfToken").value
+        
+        await fetch(`/update_score/${assignment_id}/${answer_id}`, {
+            method: 'PUT',
+            headers:{'X-CSRFToken': csrfToken},
+            body: JSON.stringify({
+                new_score: element.value
+            })
+        })
+    })
+    setTimeout(() => {
+        location.href = `/teacher_results/${test_id}`;
+    }, 500);
+    
+    return false;
+}
